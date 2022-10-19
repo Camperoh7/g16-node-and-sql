296212b844d0a95480d6a89eb2b3af902376e856
Camperoh7 <gibran_h7@hotmail.com>
2022-10-19 10:59:05 -0500

    sync-dotenv

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.send({ server: 'Express with SQL' });
+router.get('/api/v1', function(req, res, next) {
+  res.send({ server: 'Express with SQL'})
+});
+
+router.get('/health', function(req, res, next) {
+  res.send({ status: 'Ok'})
 });
 
 module.exports = router;
